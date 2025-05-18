08680d889e5700e7dcdcb7fc806d18bc0fefa3fb
richard <104408524+richart-coder@users.noreply.github.com>
2025-05-18 11:47:08 +0800

    chore: setup jest testing environment

---
+++ src/services/CartService.js
@@ -0,0 +1,5 @@
+const CartService = {
+  // 這裡將添加購物車相關的方法
+};
+
+export default CartService;
---
+++ src/services/CartService.test.js
@@ -0,0 +1,7 @@
+import CartService from "./CartService";
+
+describe("CartService", () => {
+  it("should be defined", () => {
+    expect(CartService).toBeDefined();
+  });
+});
